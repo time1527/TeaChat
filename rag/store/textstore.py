b6e6d8a9c00b7e5efa9924d36f96a5ec3855c82a
WanpengXu <xwp@cumt.edu.cn>
2024-04-21 20:53:24 +0800

    update textstore.py

--- rag/store/textstore.py
+++ rag/store/textstore.py
@@ -1,9 +1,6 @@
 import os
 import sys
 sys.path.append("../.")
-
-import PyPDF2
-
 from langchain_community.document_loaders import JSONLoader
 from langchain_community.vectorstores import Chroma
 # from langchain_community.retrievers import BM25Retriever
@@ -21,7 +18,7 @@ class TextStore:
         self.major = major
         self.json_path = f'../../rag_data/text/{self.major}_directory.json'
         # self.embedding_path = '/Users/wanpengxu/Github/bge-base-zh-v1.5'
-        self.embedding_path = HuggingFaceEmbeddings(
+        self.embedding = HuggingFaceEmbeddings(
                                 model_name = 'maidalun1020/bce-embedding-base_v1',
                                 encode_kwargs = {'normalize_embeddings': True})
         self.db_path = f'./{self.major}_text_db'
@@ -31,7 +28,7 @@ class TextStore:
         if os.path.exists(self.db_path):
             self.vectordb = Chroma(
                 persist_directory=self.db_path, 
-                embedding_function=self.embedding_path
+                embedding_function=self.embedding
             )
         else:
             self._init_docs_and_db()
@@ -39,22 +36,39 @@ class TextStore:
         # self.bm25retriever = BM25Retriever.from_documents(self.docs)
         # self.retriever = EnsembleRetriverReranker(self.docs)
 
-        self.template = """使用以下参考资料来回答用户的问题。在回答的最后给出使用了的参考资料，例如：“参考book的st页到ed页”。总是使用中文回答。
-        问题: {question}
-        参考资料: 
-        ···
-        {context}
-        ···
-        如果给定的上下文无法让你做出回答，请回答你不知道。
-        有用的回答:"""
+        # self.template = """使用以下参考资料来回答用户的问题。在回答的最后给出使用了的参考资料，例如：“参考book的st页到ed页”。总是使用中文回答。
+        # 问题: {question}
+        # 参考资料: 
+        # ···
+        # {context}
+        # ···
+        # 如果给定的上下文无法让你做出回答，请回答你不知道。
+        # 有用的回答:"""
 
-        self.QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template=self.template)
+        # self.QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template=self.template)
 
     def _metadata_func(self, record: dict, metadata: dict) -> dict:
         ob = record.get('info')
         metadata['st'] = ob["st"]
         metadata['ed'] = ob["ed"]
         metadata['book'] = ob["book"]
+
+        pdf_path = f'../../rag_data/textbook/{ob["book"]}'
+
+        # 如果 json 里有就不需要在这里 extract
+        content = ''
+
+        with open(pdf_path, 'rb') as input_file:
+            pdf_reader = PyPDF2.PdfReader(input_file)
+
+            for page_number in range(ob["st"], ob["ed"]+1):
+                page = pdf_reader.pages[page_number]
+                page_content = page.extract_text()
+
+                content += page_content
+        
+        metadata['content'] = content
+
         return metadata
 
     def _init_docs_and_db(self) -> None:
@@ -69,7 +83,7 @@ class TextStore:
 
         self.vectordb = Chroma.from_documents(
             documents = self.docs,
-            embedding = self.embedding_path,
+            embedding = self.embedding,
             persist_directory = self.db_path
         )
         self.vectordb.persist()
@@ -86,28 +100,24 @@ class TextStore:
 
         retrieval_info = retriever.get_relevant_documents(question)[0].metadata
 
-        ## 从 metadata 中提取 st 和 ed，PyPDF2 提取文字
+        ## 从 metadata 中提取 st 和 ed，PyPDF2 抽取文字
         pdf_name = retrieval_info['book']
-        pdf_path = f'../../rag_data/textbook/{pdf_name}'
         st = retrieval_info['st']
         ed = retrieval_info['ed']
-
-        content = ''
-
-        with open(pdf_path, 'rb') as input_file:
-            pdf_reader = PyPDF2.PdfReader(input_file)
-
-            for page_number in range(st, ed+1):
-                page = pdf_reader.pages[page_number]
-                page_content = page.extract_text()
-
-                content += page_content
+        context = retrieval_info['content']
+        
+        prompt = f"""使用以下参考资料来回答用户的问题。在回答的最后给出使用了的参考资料，例如：“参考book的st页到ed页”。总是使用中文回答。
+        问题: {question}
+        参考资料: 
+        ···
+        {context}
+        ···
+        如果给定的上下文无法让你做出回答，请回答你不知道。
+        有用的回答:"""
 
         ## 放到 Prompt 里进行 QA
-        # llm = InternLM(model_path="/root/share/new_models/Shanghai_AI_Laboratory/internlm2-chat-1_8b")
-        # llm.predict()
-
-
+        llm = InternLM(model_path="/root/share/new_models/Shanghai_AI_Laboratory/internlm2-chat-1_8b")
+        return llm(prompt)
 
         ## 检索-问答
         # llm = InternLM(model_path = "/root/model/Shanghai_AI_Laboratory/internlm-chat-7b")
@@ -126,5 +136,4 @@ class TextStore:
 
 if __name__ == "__main__":
     vb = TextStore("chemistry")
-    # print(vb.query("氧化还原反应"))
-    vb.query("氧化还原反应")
+    print(vb.query("氧化还原反应"))
