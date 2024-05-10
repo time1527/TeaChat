93744cf70aedd562b6b6a13ddc0d3862ccdd971b
time1527 <missethel@163.com>
2024-05-10 15:53:55 +0800

    clean data for 7b

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -11,10 +11,10 @@
 #     --input_dir /home/pika/Dataset/minsft/ \
 #     --test_dir /home/pika/Dataset/opencompass/data/
 
-python /home/pika/Project/TeaChat/data/pt_main.py \
-    --input_dir /home/pika/Dataset/pt/ \
-    --test_dir /home/pika/Dataset/opencompass/data/ \
-    --nf_threshold 100
+# python /home/pika/Project/TeaChat/data/pt_main.py \
+#     --input_dir /home/pika/Dataset/pt/ \
+#     --test_dir /home/pika/Dataset/opencompass/data/ \
+#     --nf_threshold 100
 
 python /home/pika/Project/TeaChat/data/sft_main.py \
     --input_dir /home/pika/Dataset/sft/ \
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-path=/home/pika/Model/Shanghai_AI_Laboratory/internlm2-chat-1_8b
+# path=/home/pika/Model/Shanghai_AI_Laboratory/internlm2-chat-1_8b
+path=/home/pika/Downloads/second/final_model/final40000
 lmdeploy serve api_server "$path" --model-name internlm2-chat-1_8b --model-format hf --quant-policy 0 --server-name 127.0.0.1 --server-port 23333  --tp 1 --cache-max-entry-count 0.1 & sleep 30s
 python /home/pika/Project/TeaChat/gradio_app.py 
